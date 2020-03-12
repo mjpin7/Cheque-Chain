e05d0d0bf8e7ae644451cc801fd84fdfd6025625
MJ <mjpin7@gmail.com>
2020-03-12 12:19:25 -0400

    Changed display of transaction

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getBsProps } from 'react-bootstrap/lib/utils/bootstrapUtils';
+import ConductTransaction from './ConductTransaction';
 
 const Transaction = ({ transaction }) => {
-  const { input, outputMap } = transaction;
+  const { input, outputMap,datamap} = transaction;
   const recipients = Object.keys(outputMap);
-
+  
+ 
   return (
     <div className='Transaction'>
       <div>From: {`${input.address.substring(0, 20)}...`} | Balance: {input.amount}</div>
+      <div>finInstNum: {`${datamap.finInstNum}`}</div>
+      <div>accountId: {`${datamap.accountId}`}</div>
+      <div>tranNum: {`${datamap.tranNum}`}</div>
+      <div>date: {`${datamap.date}`}</div>
       {
         recipients.map(recipient => (
           <div key={recipient}>
             To: {`${recipient.substring(0, 20)}...`} | Sent: {outputMap[recipient]}
+           
           </div>
         ))
       }
+     
+      
+    
     </div>
+    
   );
+  // <div key = {finInstNum}> finInstNum: {updatefinInstNum}</div>
+  //     <div key = {accountId}> accountId: {updateaccountId}</div>
+  //     <div key = {transNum}> transNum: {updatetransNum}</div>
+  //     <div key = {date}> date: {updatedate}</div>
+      
 }
 
 export default Transaction;
